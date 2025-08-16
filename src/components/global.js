d8b1a30810d300b49f24d48b9e2c74a241e00428
napu <nahuel.haberkorn@gmail.com>
2025-08-16 12:42:37 -0300

    test

--- src/components/global.js
+++ src/components/global.js
@@ -1,2 +1,5 @@
 export default function () {
+    performance.mark('target-point')
+    performance.measure('start-to-target', 'start-point', 'target-point')
+    console.log(performance.getEntriesByName('start-to-target')[0].duration, 'ms')
 }
